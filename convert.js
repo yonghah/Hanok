65a7dde882d59255d035795c3d19dfcdc2bf3a1a
Yongha Hwang <yonghah@umich.edu>
2016-02-11 23:40:28 -0500

    initial commit

---
+++ convert.js
@@ -0,0 +1,25 @@
+"use strict";
+var fs = require('fs');
+var request = require('request');
+var _ = require("underscore");
+
+var hdataUrl = "http://www.hanokdb.kr/data/map/data.map.test.js";
+request(hdataUrl, function(err, resp, html){
+	let hdataRaw = resp.body.replace("var data =", "");
+	hdataRaw = hdataRaw.replace(";", "");
+	let hdata = JSON.parse(hdataRaw).hanok;
+	createGeoJson(hdata);
+});
+
+function createGeoJson(data) {
+	let gj = {type:"FeatureCollection", features:[]};
+	_.each(data, function(row){
+		let feature = {
+			type:"Feature", 
+			geometry:{type:"Point", coordinates:[+row.longitude, +row.latitude]},
+			properties:row
+		};
+		gj.features.push(feature);
+	});
+	fs.writeFile("hanok.geojson", JSON.stringify(gj, null, 4));
+}
